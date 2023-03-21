242fa95336809ef7426e6889fe8576697416d9d8
govindamaheshwari <125073849+govindamaheshwari@users.noreply.github.com>
2023-03-21 13:34:23 +0530

    Add files via upload
    Project-EJS

--- Express.js/shop.js
+++ Express.js/shop.js
@@ -1,8 +1,20 @@
-const express=require('express');
-const path=require('path');
-const router=express.Router();
+const path = require('path');
 
-router.get('/',(req,res,next)=>{
-        res.sendFile(path.join(__dirname,'../','views','shop.html'));
-})
-module.exports=router
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+router.get('/products/:productId',shopController.getProduct)
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
