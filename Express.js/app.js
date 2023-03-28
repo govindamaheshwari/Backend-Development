d8098b4ae57fb4d50457accb1bf412a15cd7b75e
govindamaheshwari <125073849+govindamaheshwari@users.noreply.github.com>
2023-03-28 13:25:22 +0530

    Add files via upload

--- Express.js/app.js
+++ Express.js/app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db=require('./util/database');
+const sequelize=require('./util/database');
+const User=require('./models/user.js')
+const Product=require('./models/product.js')
 
 const app = express();
 
@@ -13,14 +15,35 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// db.execute('SELECT * FROM products').then(([rows,fieldData])=>
-// console.log(fieldData)).catch((err)=>console.log(err))
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next)=>{
+    User.findAll({where:{id:1}})
+    .then(user=>{
+        req.user=user[0];
+        next()
+    
+    }).catch((err)=>console.log(err))
+
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
+sequelize.sync().then(user=>{return User.findAll({where:{id:1}})})
+.then((user)=>{
+    if(!user[0]){
+        return User.create({name:'Max',email:'max@gmail.com'})
+    }
+    return user[0]
+})
+.then(user=>
+{console.log(user)
+app.listen(3000)}
+).catch((err) =>console.log(err))
 
-app.listen(3000);
+
